f59477255128eadf4cce778de25188efb18725a6
Yihsiu <isho.lee@gmail.com>
2018-11-09 22:56:29 +0800

    unit test

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ require('./startup/validation')();
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
-///////// Throw an exception for demo
+// --------- Throw an exception for demo
 // throw new Error('Something failed during startup.');
-//////// Make a promise rejection for demo
+// --------- Make a promise rejection for demo
 // const p = Promise.reject(new Error('Something failed miserably!'));
 // p.then(() => console.log('Done'));
