44cf2f58a3a80a4d44a16a513516541bcb384714
Yihsiu <isho.lee@gmail.com>
2018-11-10 21:29:51 +0800

    set up test database

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
-module.exports = function() {
+module.exports = function () {
+  const db = config.get('db')
   mongoose
-    .connect('mongodb://localhost/vidly')
-    .then(() => winston.info('Connected to MongoDB...'));
+    .connect(db)
+    .then(() => winston.info(`Connected to ${db}...`));
 };
