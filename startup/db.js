7763044f37043bc3fabd4a6b49ac0e9dd5b0c426
Dave Lee <isho.lee@gmail.com>
2019-01-09 11:31:35 +0800

    change for wise-paas

--- startup/db.js
+++ startup/db.js
@@ -8,3 +8,22 @@ const config = require('config');
 //     .connect(db)
 //     .then(() => winston.info(`Connected to ${db}...`));
 // };
+
+// For Wise-PaaS to work
+module.exports = function () {
+  vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+  console.log(vcap_services);
+  // console.log(VCAP_SERVICES);
+  const db = vcap_services['mongodb'][0].credentials.uri + '?replicaSet=rs0';
+  // const mongo_database = vcap_services['mongodb-develop'][0].credentials.database;
+  // const mongo_host1 = vcap_services['mongodb-develop'][0].credentials.host1;
+  // const mongo_port1 = vcap_services['mongodb-develop'][0].credentials.port1;
+  // const mongo_user = vcap_services['mongodb-develop'][0].credentials.username;
+  // const mongo_password = vcap_services['mongodb-develop'][0].credentials.password;
+
+  mongoose
+    .connect(db)
+    .then(() => winston.info(`Connected to ${db}...`));
+};
+
+
